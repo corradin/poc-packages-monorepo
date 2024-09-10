b57b4979192e0102ce297f6a3b3da967fd106091
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2024-09-10 11:47:53 +0200

    breaking change and updates

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,19 +1,15 @@
-"use client";
+'use client';
 
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
 interface ButtonProps {
   children: ReactNode;
   className?: string;
-  appName: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className }: ButtonProps) => {
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
+    <button className={className} onClick={() => alert(`Hello from your app!`)}>
       {children}
     </button>
   );
