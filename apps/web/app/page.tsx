0fe97ba7c4f063d9839561eff3fb2e41521c8406
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2024-09-10 11:46:58 +0200

    entry point

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import { Button } from '@corradin/poc-packages-monorepo-ui/dist';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
